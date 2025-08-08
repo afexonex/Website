e7ddb4dbef7afc9cff02e8c4c2ef82802438110f
afexonex <shopifyauto9@gmail.com>
2025-08-08 08:44:39 +0530

    Create footer.php

---
+++ headers/footer.php
@@ -0,0 +1,16 @@
+<footer class="content-footer footer bg-footer-theme">
+  <div class="container-xxl">
+    <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+      <div>
+        © <script>
+          document.write(new Date().getFullYear())
+        </script>, THIS PROJECT MADE BUY SEMU 💯<a href="https://t.me/SemuPremiumBot" target="_blank" class="fw-medium">Admin</a>
+      </div>
+      <div class="d-none d-lg-inline-block">
+        <a href="https://t.me/+TcoCddhC_gJjM2Y9" target="_blank" class="footer-link me-4">Channel</a>
+        <a href="http://t.me/SemuPremiumBot" target="_blank" class="footer-link me-4">Admin</a>
+        <a href="http://t.me/SemuPremiumBot" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+      </div>
+    </div>
+  </div>
+</footer>
